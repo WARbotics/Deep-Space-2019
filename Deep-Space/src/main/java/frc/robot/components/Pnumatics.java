9995cb1f9c8aa489acb198138e72de04a724b372
Victor Henriksson <vhenrixon@gmail.com>
2019-02-19 16:02:50 -0500

    BUG in pnumatics

--- Deep-Space/src/main/java/frc/robot/components/Pnumatics.java
+++ Deep-Space/src/main/java/frc/robot/components/Pnumatics.java
@@ -1,12 +1,10 @@
 package frc.robot.components;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.Timer;
 
 public class Pnumatics{
 
     public DoubleSolenoid m_solenoid = new DoubleSolenoid(0,1);
-    public DoubleSolenoid.Value state; 
     double latest = 0;
     public void setFoward(){
         m_solenoid.set(DoubleSolenoid.Value.kForward);
@@ -19,7 +17,7 @@ public class Pnumatics{
         m_solenoid.set(DoubleSolenoid.Value.kOff);
     }
     public boolean isOpen(){
-        if(state == DoubleSolenoid.Value.kForward){
+        if(m_solenoid.get() == DoubleSolenoid.Value.kForward){
             return true;
         }else{
             return false;
